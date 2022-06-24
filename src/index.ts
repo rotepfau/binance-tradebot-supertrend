dc20a2dc51570c0d6a84f22c722d48ac8476f9ad
rotepfau <rotepfau@gmail.com>
2022-06-23 23:02:25 -0400

    new feature

--- src/index.ts
+++ src/index.ts
@@ -6,14 +6,9 @@ import { SuperTrend, EMA } from "@debut/indicators";
 const supertrend = new SuperTrend(ST.PERIOD, ST.MULTIPLIER);
 const ema = new EMA(E.PERIOD);
 
-// leverage config setup
-client.futuresLeverage({
-	symbol: PROP.pair,
-	leverage: PROP.leverage,
-});
-
 // set previous candles on indicators
 (async () => {
+	console.info("reading previous candles");
 	const pastCandles = await client.futuresCandles({
 		symbol: PROP.pair,
 		interval: PROP.interval,
@@ -28,66 +23,123 @@ client.futuresLeverage({
 		ema.nextValue(Number(close));
 		if (i === pastCandles.length - 1) prevSupertrendDir = st.direction;
 	}
-	return console.info("past candles set");
+	console.info("past candles set");
+	// start to hear future candles
+	console.info("started to hear for new candles");
+	client.ws.futuresCandles(PROP.pair, PROP.interval, (candle) => {
+		let { high, low, close, isFinal } = candle;
+		// trigger when candle close
+		if (isFinal) {
+			Main(Number(high), Number(low), Number(close));
+		}
+	});
 })();
 
-// hear for websocket binance
-client.ws.futuresCandles(PROP.pair, PROP.interval, (candle) => {
-	let { high, low, close, isFinal } = candle;
-	// trigger when candle close
-	if (isFinal) {
-		Main(Number(high), Number(low), Number(close));
-	}
+// leverage config setup
+client.futuresLeverage({
+	symbol: PROP.pair,
+	leverage: PROP.leverage,
 });
 
-let bought: boolean = false;
+// hear for websocket binance
+
+let position: "opened" | "closed" = "closed";
+let direction: "short" | "long" | "" = "";
 let supertrendDir: number | undefined = undefined;
 let prevSupertrendDir: number | undefined = undefined;
+let lastAvgPrice: number = 0;
 // run every candle
 const Main = (h: number, l: number, c: number) => {
 	const st = supertrend.nextValue(h, l, c);
 	const e = ema.nextValue(c);
 	supertrendDir = st ? st.direction : undefined;
-	console.info("bought", bought);
+	console.info("position", position);
+	console.info("direction", direction);
 	console.info("supertrend", st);
 	console.info("ema", e);
 	console.info("close", c);
-	if (
-		bought === false &&
-		c > e &&
-		prevSupertrendDir === 1 &&
-		supertrendDir === -1
-	) {
-		console.info("Buyin");
-		placeOrder("BUY");
+	if (position === "closed") {
+		if (
+			direction !== "long" &&
+			c > e &&
+			prevSupertrendDir === 1 &&
+			supertrendDir === -1
+		) {
+			console.info("Open Long");
+			placeOrder("BUY");
+			position = "opened";
+			direction = "long";
+		}
+		if (
+			direction !== "short" &&
+			c < e &&
+			prevSupertrendDir === -1 &&
+			supertrendDir === 1
+		) {
+			console.info("Open Short");
+			placeOrder("SELL");
+			position = "opened";
+			direction = "short";
+		}
 	}
-	if (
-		bought === true &&
-		c < e &&
-		prevSupertrendDir === -1 &&
-		supertrendDir === 1
-	) {
-		console.info("Selling");
-		placeOrder("SELL");
+
+	if (position === "opened") {
+		if (
+			direction === "long" &&
+			c < e &&
+			prevSupertrendDir === -1 &&
+			supertrendDir === 1
+		) {
+			console.info("Close Long");
+			placeOrder("SELL");
+			position = "closed";
+			direction = "";
+		}
+		if (
+			direction === "short" &&
+			c > e &&
+			prevSupertrendDir === 1 &&
+			supertrendDir === -1
+		) {
+			console.info("Close Short");
+			placeOrder("BUY");
+			position = "closed";
+			direction = "";
+		}
+		if (direction === "long" && c >= lastAvgPrice * 1.005) {
+			console.info("Close Long");
+			placeOrder("SELL");
+			position = "closed";
+			direction = "";
+		}
+		if (direction === "short" && c <= lastAvgPrice * 0.995) {
+			console.info("Close Short");
+			placeOrder("BUY");
+			position = "closed";
+			direction = "";
+		}
 	}
+
 	prevSupertrendDir = supertrendDir;
 };
 
+// place order
 async function placeOrder(position: OrderSide_LT) {
-	if ((await getBalance()) >= PROP.balance || bought) {
+	if ((await getBalance()) >= PROP.balance) {
 		const order = await client.futuresOrder({
 			symbol: PROP.pair,
 			side: position,
 			quantity: PROP.balance.toString(),
 			type: "MARKET",
 		});
-		bought = !bought;
+		lastAvgPrice = Number(order.avgPrice);
 		return console.info(order);
 	} else {
 		return console.error("Low balance");
 	}
 }
 
+// get current future account balance and convert to BTC
 async function getBalance() {
 	const balance = await client.futuresAccountBalance();
 	const symbol = balance.filter((el) => {
